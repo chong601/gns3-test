543593ee83647b2bc2477e08eac7c8b2eb25c2f0
Chong Jin Yi <chong601.13@gmail.com>
2024-10-07 20:06:43 +0800

    Integrate AS64500 network

---
+++ AS64500-DNS3/frr.sh
@@ -0,0 +1,27 @@
+ip forwarding
+ipv6 forwarding
+
+router ospf 10
+ospf router-id 172.31.210.135
+passive-interface default
+
+interface ens3
+ip address 172.31.210.135/26
+ip ospf 10 area 0
+no ip ospf passive
+
+interface ens4
+ip address 172.31.210.136/26
+ip ospf 10 area 0
+no ip ospf passive
+
+router bgp 64500
+# BEGIN APNIC tuning
+distance bgp 200 200 200
+bgp deterministic-med
+# END APNIC tuning
+
+neighbor 172.31.210.0 remote-as 64500
+neighbor 172.31.210.0 update-source ens3
+neighbor 172.31.210.1 remote-as 64500
+neighbor 172.31.210.1 update-source ens4
---
+++ AS64500-R2.sh
@@ -0,0 +1,70 @@
+set system host-name AS64500-R2
+set system name-server 8.8.8.8
+set system name-server 8.8.4.4
+set service ssh
+
+set interfaces dummy dum0 address 172.31.210.1/32
+set interfaces ethernet eth0 address 172.31.210.130/26
+set interfaces ethernet eth6 address 172.31.210.3/31
+set interfaces ethernet eth7 address 172.31.210.6/31
+
+set protocols ospf parameters router-id 172.31.210.1
+set protocols ospf passive-interface default
+set protocols ospf log-adjacency-changes
+
+set protocols ospf interface dum0 area 0
+set protocols ospf interface eth0 area 0
+set protocols ospf interface eth6 area 0
+set protocols ospf interface eth7 area 0
+
+set protocols ospf interface eth0 passive disable
+set protocols ospf interface eth6 passive disable
+
+set protocols ospf interface eth6 network point-to-point
+
+set protocols bgp system-as 64500
+
+# standard BGP parameter tuning per recommendation by APNIC
+set protocols bgp parameters router-id 172.31.210.1
+set protocols bgp parameters distance global external 200
+set protocols bgp parameters distance global internal 200
+set protocols bgp parameters distance global local 200
+set protocols bgp parameters deterministic-med
+
+set protocols bgp neighbor 172.31.210.7 remote-as 64854
+set protocols bgp neighbor 172.31.210.7 address-family ipv4-unicast
+
+set protocols bgp neighbor 172.31.210.0 remote-as 64500
+set protocols bgp neighbor 172.31.210.0 update-source dum0
+set protocols bgp neighbor 172.31.210.0 address-family ipv4-unicast nexthop-self
+set protocols bgp neighbor 172.31.210.129 remote-as 64500
+set protocols bgp neighbor 172.31.210.129 passive
+set protocols bgp neighbor 172.31.210.129 update-source dum0
+set protocols bgp neighbor 172.31.210.129 address-family ipv4-unicast nexthop-self
+set protocols bgp neighbor 172.31.210.131 remote-as 64500
+set protocols bgp neighbor 172.31.210.131 passive
+set protocols bgp neighbor 172.31.210.131 update-source dum0
+set protocols bgp neighbor 172.31.210.131 address-family ipv4-unicast nexthop-self
+set protocols bgp neighbor 172.31.210.132 remote-as 64500
+set protocols bgp neighbor 172.31.210.132 passive
+set protocols bgp neighbor 172.31.210.132 update-source dum0
+set protocols bgp neighbor 172.31.210.132 address-family ipv4-unicast nexthop-self
+set protocols bgp neighbor 172.31.210.133 remote-as 64500
+set protocols bgp neighbor 172.31.210.133 passive
+set protocols bgp neighbor 172.31.210.133 update-source dum0
+set protocols bgp neighbor 172.31.210.133 address-family ipv4-unicast nexthop-self
+set protocols bgp neighbor 172.31.210.134 remote-as 64500
+set protocols bgp neighbor 172.31.210.134 passive
+set protocols bgp neighbor 172.31.210.134 update-source dum0
+set protocols bgp neighbor 172.31.210.134 address-family ipv4-unicast nexthop-self
+set protocols bgp neighbor 172.31.210.135 remote-as 64500
+set protocols bgp neighbor 172.31.210.135 passive
+set protocols bgp neighbor 172.31.210.135 update-source dum0
+set protocols bgp neighbor 172.31.210.135 address-family ipv4-unicast nexthop-self
+set protocols bgp neighbor 172.31.210.136 remote-as 64500
+set protocols bgp neighbor 172.31.210.136 passive
+set protocols bgp neighbor 172.31.210.136 update-source dum0
+set protocols bgp neighbor 172.31.210.136 address-family ipv4-unicast nexthop-self
+
+set protocols bgp address-family ipv4-unicast network 172.31.210.0/24
+set protocols static route 172.31.210.0/24 blackhole
